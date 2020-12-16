d7d47af56d60f38dc7fd7d27fba2419e54b97425
mephi007 <roysumit949@gmail.com>
2020-12-17 02:23:07 +0530

    creating Factory method design pattern

--- src/main/java/org/example/Checkout.java
+++ src/main/java/org/example/Checkout.java
@@ -1,15 +1,22 @@
 package org.example;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.*;
 
 public class Checkout {
+//    public static Map<String, IPromotions> getPromotionRuleObject = new HashMap();
+//    getPromotionRuleObject.put("A", new Checkout());
+
     public void processItem(Map<String, Integer> cartList) {
         double payableAmt = 0;
         IPromotions promtionsRule = null;
         for(Entry<String,Integer> pr: cartList.entrySet()){
-
+            if(pr.getKey().equals("A")){
+                promtionsRule = new PromotionRuleA();
+            }else if(pr.getKey().equals("B")){
+                promtionsRule = new PromotionRuleB();
+            }
         }
     }
 }
