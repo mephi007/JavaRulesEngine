c0dddeee8bd5582326b309da94bcbbd5696b3ddf
mephi007 <roysumit949@gmail.com>
2020-12-17 02:39:16 +0530

    create class to contain Promotion Rule

---
+++ src/main/java/org/example/PromotionRule.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PromotionRule {
+    private static Map<String, Integer> getPromotionRate = new HashMap();
+
+    public PromotionRule(){
+        getPromotionRate.put("A", 50);
+        getPromotionRate.put("B", 30);
+        getPromotionRate.put("C", 20);
+        getPromotionRate.put("D", 15);
+    }
+    public void setSKUIDwithRate(String skuId, int rate){
+        getPromotionRate.put(skuId, rate);
+    }
+    public int getSKUIDRate(String skuId){
+        return getPromotionRate.getOrDefault(skuId,0);
+    }
+}
