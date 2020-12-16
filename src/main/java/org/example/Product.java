665900825d0aae576aa2817a771673128eff172b
mephi007 <roysumit949@gmail.com>
2020-12-16 18:49:36 +0530

    Added getter and Setter for items

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,12 +1,28 @@
 package org.example;
 
 
-public class Cart {
-    SKU SkuId;
-    int quantity;
+public class Product {
+    private SKU SkuId;
+    private int quantity;
 
-    public Cart(SKU skuid, int qu){
+    public Product(SKU skuid, int qu){
         this.SkuId = skuid;
         this.quantity = qu;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public SKU getSkuId() {
+        return SkuId;
+    }
+
+    public void setSkuId(SKU skuId) {
+        SkuId = skuId;
     }
 }
