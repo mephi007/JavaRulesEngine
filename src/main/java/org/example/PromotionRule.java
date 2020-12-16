caa4f4844fd80585c8fe5eb55b40aaa283743153
mephi007 <roysumit949@gmail.com>
2020-12-17 02:46:36 +0530

    making Promotion Rule as Singleton Class

--- src/main/java/org/example/PromotionRule.java
+++ src/main/java/org/example/PromotionRule.java
@@ -4,15 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PromotionRule {
-    private static Map<String, Integer> getPromotionRate = new HashMap();
-
-    public PromotionRule(){
+    private Map<String, Integer> getPromotionRate = new HashMap();
+    private static PromotionRule promotionRule;
+    private PromotionRule(){
         getPromotionRate.put("A", 50);
         getPromotionRate.put("B", 30);
         getPromotionRate.put("C", 20);
         getPromotionRate.put("D", 15);
         getPromotionRate.put("3A", 130);
         getPromotionRate.put("2B", 45);
+    }
+    public static PromotionRule getInstance(){
+        if(promotionRule == null){
+            promotionRule = new PromotionRule();
+        }
+        return promotionRule;
     }
     public void setSKUIDwithRate(String skuId, int rate){
         getPromotionRate.put(skuId, rate);
