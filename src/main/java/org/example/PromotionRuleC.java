fcb827b539eab9b3b02946322e60ca4555055782
mephi007 <roysumit949@gmail.com>
2020-12-17 03:24:40 +0530

    created classes for Promotion Rule C, D and CD

---
+++ src/main/java/org/example/PromotionRuleC.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class PromotionRuleC implements IPromotions {
+    @Override
+    public double applyPromotionRules(int quantity) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/PromotionRuleD.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class PromotionRuleD implements IPromotions {
+    @Override
+    public double applyPromotionRules(int quantity) {
+        return 0;
+    }
+}
