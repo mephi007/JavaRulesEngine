a88210fabc16f29dacf67242d3c2b947d6494735
mephi007 <roysumit949@gmail.com>
2020-12-17 02:50:34 +0530

    written promotion rule for B

--- src/main/java/org/example/PromotionRuleB.java
+++ src/main/java/org/example/PromotionRuleB.java
@@ -3,6 +3,9 @@ package org.example;
 public class PromotionRuleB implements IPromotions {
     @Override
     public double applyPromotionRules(int quantity) {
-        return 0;
+        int bundleOfTwo = quantity/2;
+        int remainder = quantity%2;
+        PromotionRule promotionRule = PromotionRule.getInstance();
+        return bundleOfTwo*promotionRule.getSKUIDRate("2B") + remainder*promotionRule.getSKUIDRate("B");
     }
 }
